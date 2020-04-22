cae4f7e4a9063010fd0564ca2239ca0cee0f1ca7
Jonas R. Denardi <36958021+jonasrdenardi@users.noreply.github.com>
2020-04-22 17:58:38 -0300

    Inicio-Componentes
    Do início até a criação dos componentes

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Seus Componentes
+  ],
+  imports: [
+    CommonModule
+    // Seus Modulos
+  ],
+  providers: 
+  [
+    // Seus Servicos
+  ]
+})
+export class FuncionalidadeModule { }
